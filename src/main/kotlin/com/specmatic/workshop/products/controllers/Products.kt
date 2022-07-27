7a70305a4de281ab4ac594a4267031e1299c2f46
Joel Rosario <cirquitz@gmail.com>
2022-07-27 20:29:35 +0530

    fixes
    - Updated .gitignore to ignore .DS_Store
    - Fixed Java version to 11
    - Added an API

--- src/main/kotlin/com/specmatic/workshop/products/controllers/Products.kt
+++ src/main/kotlin/com/specmatic/workshop/products/controllers/Products.kt
@@ -2,12 +2,18 @@ package com.specmatic.workshop.products.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class Products {
     @GetMapping("/products/{productId}")
     fun details(@PathVariable productId: String) = Product("book", "book_sku4")
+
+    @PostMapping("/products")
+    fun add(@RequestBody product: Product) = Id()
 }
 
+data class Id(val id: Int = (0..1000000).random())
 data class Product(val name: String, val sku: String)
